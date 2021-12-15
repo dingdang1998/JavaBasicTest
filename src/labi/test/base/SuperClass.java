8bc139d1807047a1aa68dcd1b030a344f9f5a244
dzp <2497352182@qq.com>
2021-12-15 14:24:30 +0800

    测试111

---
+++ src/labi/test/base/SuperClass.java
@@ -0,0 +1,13 @@
+package labi.test.base;
+
+/**
+ * @program: JavaBasicTest
+ * @description: 测试数组中的类中有静态代码块
+ * @author: dzp
+ * @create: 2021-12-15 13:55
+ **/
+public class SuperClass {
+    static {
+        System.out.println("11111111111");
+    }
+}
---
+++ src/labi/test/base/Test.java
@@ -0,0 +1,15 @@
+package labi.test.base;
+
+/**
+ * @program: JavaBasicTest
+ * @description: 所有测试在这里面进行
+ * @author: dzp
+ * @create: 2021-12-15 14:00
+ **/
+public class Test {
+
+    public static void main(String[] args) {
+        SuperClass[] arrays = new SuperClass[]{};
+        System.out.println(arrays.toString());
+    }
+}
