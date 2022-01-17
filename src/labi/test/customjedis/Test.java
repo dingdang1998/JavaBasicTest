84f2a49f91762693ee9ebf79790bcf49cd725000
dzp <2497352182@qq.com>
2022-01-17 14:24:20 +0800

    jedis引入

---
+++ src/labi/test/customjedis/Test.java
@@ -0,0 +1,13 @@
+package labi.test.customjedis;
+
+/**
+ * @program: JavaBasicTest
+ * @description:
+ * @author: dzp
+ * @create: 2022-01-13 09:51
+ **/
+public class Test {
+    public static void main(String[] args) {
+//        JedisCluster jedisCluster = new JedisCluster();
+    }
+}
