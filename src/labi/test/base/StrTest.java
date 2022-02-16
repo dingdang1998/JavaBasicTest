0a9330d0b02de2c94639f9e2f4ea2b1260721f13
dzp <2497352182@qq.com>
2022-02-16 10:04:48 +0800

    22/2/16 10:04提交

--- src/labi/test/base/StrTest.java
+++ src/labi/test/base/StrTest.java
@@ -1,5 +1,7 @@
 package labi.test.base;
 
+import cn.hutool.core.util.StrUtil;
+
 /**
  * @program: JavaBasicTest
  * @description:
@@ -8,7 +10,21 @@ package labi.test.base;
  **/
 public class StrTest {
     public static void main(String[] args) {
-        String s = "aaaaa a";
-        System.out.println(s.lastIndexOf('a'));
+        String s = "0,0,0,0,0,0";
+
+        System.out.println(s.equals("1970,01,01,00,00,00") || s.equals("0000,00,00,00,00,00") || s.equals("0,0,0,0,0,0") ? "--" : "xx");
+
+        String s1 = "hhhhhh";
+        String s2 = "aaaaaaaaa";
+        System.out.println(s1 + "\r\n" + s2);
+
+//        String s3 = null;
+//        System.out.println(s3.length());
+
+        int i = Runtime.getRuntime().availableProcessors();
+        System.out.println(i);
+
+        String s4 = "";
+        System.out.println(StrUtil.isBlankIfStr(s4));
     }
 }
--- src/labi/test/customhutool/Test.java
+++ src/labi/test/customhutool/Test.java
@@ -23,37 +23,38 @@ import java.util.List;
 public class Test {
     public static void main(String[] args) {
 
-        TimeInterval timer = DateUtil.timer();
-
-        DateTime date = DateUtil.date(Calendar.getInstance());
-        System.out.println(date);
-
-        DateTime date1 = DateUtil.date();
-        System.out.println(date1);
-
-        System.out.println(timer.intervalMs());
-
-        List<TreeNode<String>> nodeList = CollUtil.newArrayList();
-
-        List<String> objects = CollUtil.newArrayList();
-
-        nodeList.add(new TreeNode<>("1", "0", "系统管理", 5));
-        nodeList.add(new TreeNode<>("11", "1", "用户管理", 222222));
-        nodeList.add(new TreeNode<>("111", "11", "用户添加", 0));
-        nodeList.add(new TreeNode<>("2", "0", "店铺管理", 1));
-        nodeList.add(new TreeNode<>("21", "2", "商品管理", 44));
-        nodeList.add(new TreeNode<>("221", "2", "商品管理2", 2));
-
-        List<Tree<String>> treeList = TreeUtil.build(nodeList, "0");
-        Console.log(treeList);
-
-        String str1 = "a, ,efedsfs,   ddf";
-        //参数：被切分字符串，分隔符逗号，0表示无限制分片数，去除两边空格，忽略空白项
-        List<String> split = StrSplitter.split(str1, ',', 2, true, true);
-        Console.log(split);
-
-        //是否为有效的端口
-        boolean result = NetUtil.isValidPort(6379);
-        Console.log(result);
+//        TimeInterval timer = DateUtil.timer();
+//
+//        DateTime date = DateUtil.date(Calendar.getInstance());
+//        System.out.println(date);
+//
+//        DateTime date1 = DateUtil.date();
+//        System.out.println(date1);
+//
+//        System.out.println(timer.intervalMs());
+//
+//        List<TreeNode<String>> nodeList = CollUtil.newArrayList();
+//
+//        List<String> objects = CollUtil.newArrayList();
+//
+//        nodeList.add(new TreeNode<>("1", "0", "系统管理", 5));
+//        nodeList.add(new TreeNode<>("11", "1", "用户管理", 222222));
+//        nodeList.add(new TreeNode<>("111", "11", "用户添加", 0));
+//        nodeList.add(new TreeNode<>("2", "0", "店铺管理", 1));
+//        nodeList.add(new TreeNode<>("21", "2", "商品管理", 44));
+//        nodeList.add(new TreeNode<>("221", "2", "商品管理2", 2));
+//
+//        List<Tree<String>> treeList = TreeUtil.build(nodeList, "0");
+//        Console.log(treeList);
+//
+//        String str1 = "a, ,efedsfs,   ddf";
+//        //参数：被切分字符串，分隔符逗号，0表示无限制分片数，去除两边空格，忽略空白项
+//        List<String> split = StrSplitter.split(str1, ',', 2, true, true);
+//        Console.log(split);
+//
+//        //是否为有效的端口
+//        boolean result = NetUtil.isValidPort(6379);
+//        Console.log(result);
+        System.out.println(DateUtil.now());
     }
 }
